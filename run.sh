ee90cb4b5644e123950a2dfef63f21c5b9946177
Drew Brantley <brantley@us.ibm.com>
2017-02-01 21:32:46 -0600

    added run to start script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 curl https://raw.githubusercontent.com/yeltnar/servo/master/untitled.py > untitled.py
+curl https://raw.githubusercontent.com/yeltnar/servo/master/run.sh > run.sh
 sudo python untitled.py
